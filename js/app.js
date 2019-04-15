22370839c2b4f8b662d4f5ef8d28e785a204f69d
Kathleen Hill <kath.hill@sbcglobal.net>
2019-04-15 11:56:37 -0700

    feat: Create separate functions to open cards and add cards to array

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 const cards = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o', 'fa-anchor', 'fa-anchor', 'fa-bolt',  'fa-bolt', 'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf', 'fa-bicycle','fa-bicycle', 'fa-bomb', 'fa-bomb'];
 
 function buildCard(card) {
-    return `<li class="card"><i class="fa ${card}"></i></li>`;
+    return `<li class="card" data-icon="${card}"><i class="fa ${card}"></i></li>`;
 }
 
 /*
@@ -52,17 +52,25 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-const getCards = document.querySelectorAll('.card');    
+let getCards = document.querySelectorAll('.card');    
 let activeCards = [];
 
 getCards.forEach (function(card) {
-    card.addEventListener('click', function(e) {
-        if (activeCards.length )
-        
-        activeCards.push(card);
-        card.classList.add('open', 'show');
+    card.addEventListener('click', function(e) {             
+        listOpenCards();
+        openShowCard(card);
     });
+    function openShowCard(card) {
+        card.classList.add('open', 'show');        
+    }
+    function listOpenCards() {
+        activeCards.push(card);
+        return activeCards;
+        // make sure no more than two cards are open 
+        
+    }
 });
+
 
 
 
